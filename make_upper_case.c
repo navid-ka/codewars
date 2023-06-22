c5513b55f7dbff54f487e101891d0a31cd49fe7b
Tu Nombre <you@example.com>
2023-06-23 01:35:30 +0200

    iso

--- make_upper_case.c
+++ make_upper_case.c
@@ -6,16 +6,18 @@
 /*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 01:49:20 by bifrost           #+#    #+#             */
-/*   Updated: 2023/06/22 01:53:36 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/06/23 01:34:52 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iso646.h>
 
 char	*make_upper_case(char *string)
 {
 	int	i = -1;
 
-	while (string[++i] != '\0')
-		if (string[i] >= 'a' && string[i] <= 'z')
+	while (string[++i] not_eq '\0')
+		if (string[i] >= 'a' and string[i] <= 'z')
 			string[i] -= 32;
 	return (string);
 }
