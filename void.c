947eeee31a688fad96485e4af04cfa827cfdaf55
Tu Nombre <you@example.com>
2023-06-30 00:25:44 +0200

    void

--- void.c
+++ void.c
@@ -9,13 +9,21 @@ void wevoid(void)
 {
     printf("MEGAVOID\n");
 }
-
+void excalivoid(void)
+{
+    printf("one void and another enters in a bar and \n", sizeof(void));
+}
+void bigsizevoid(void)
+{
+    printf("there was nothing \n", sizeof(void), sizeof(void),\
+        sizeof(void), sizeof(void));
+}
 void voidmancer(void)
 {
     long long unsigned int voidmancer = printf("void maximum\n");
     sizeof (void);
     if (voidmancer != -1)
-        return (voidance(), wevoid());
+        return (voidance(), wevoid(), excalivoid(), bigsizevoid());
 }
 
 void main(void)
