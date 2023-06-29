1668b1a86fb16d597c92474e2f0bab93519e23f3
Tu Nombre <you@example.com>
2023-06-30 00:10:10 +0200

    voidmancer

---
+++ void.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void voidance(void)
+{
+   printf("void start dancing\n");
+}
+
+void wevoid(void)
+{
+    printf("MEGAVOID\n");
+}
+
+void voidmancer(void)
+{
+    long long unsigned int voidmancer = printf("void maximum\n");
+    sizeof (void);
+    if (voidmancer != -1)
+        return (voidance(), wevoid());
+}
+
+void main(void)
+{
+    voidmancer();
+}
